276901d84d3a8356b4822be3cba786f01c6a03c0
yoshikouki <yoshikouki@pepabo.com>
2021-03-15 18:30:26 +0900

    戻り値にも型定義

--- lib/app.ts
+++ lib/app.ts
@@ -1,6 +1,8 @@
 type Operation = 'multiply' | 'add' | 'divide'
 
-const calculator = (a: number, b: number, op: Operation) => {
+type Result = number | string
+
+const calculator = (a: number, b: number, op: Operation): Result => {
   if (op === 'multiply') {
     return a * b
   } else if (op === 'add') {
