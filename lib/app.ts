a6df2ec07fd1129830dd59dd6484887a55243426
yoshikouki <yoshikouki@pepabo.com>
2021-03-15 18:22:35 +0900

    型安全な計算機

--- lib/app.ts
+++ lib/app.ts
@@ -1,3 +1,18 @@
+type Operation = 'multiply' | 'add' | 'divide'
+
+const calculator = (a: number, b: number, op: Operation) => {
+  if (op === 'multiply') {
+    return a * b
+  } else if (op === 'add') {
+    return a + b
+  } else if (op === 'divide') {
+    if (b === 0) return 'can\'t divide by 0!'
+    return a / b
+  }
+}
+
+calculator(1, 4, 'add')
+
 const multiplicator = (a, b, printText) => {
   console.log(printText,  a * b);
 }
