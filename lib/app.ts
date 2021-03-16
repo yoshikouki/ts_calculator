e9b4789de5a327ba0d9ac1aa8eb6be209c2e104f
yoshikouki <yoshikouki@pepabo.com>
2021-03-16 17:52:43 +0900

    例外処理を追加

--- lib/app.ts
+++ lib/app.ts
@@ -3,13 +3,16 @@ type Operation = 'multiply' | 'add' | 'divide'
 type Result = number | string
 
 const calculator = (a: number, b: number, op: Operation): Result => {
-  if (op === 'multiply') {
-    return a * b
-  } else if (op === 'add') {
-    return a + b
-  } else if (op === 'divide') {
-    if (b === 0) return 'can\'t divide by 0!'
-    return a / b
+  switch (op) {
+    case "multiply":
+      return a * b
+    case "add":
+      return a + b
+    case "divide":
+      if (b === 0) return '0 では割れません'
+      return a / b
+    default:
+      throw new Error('演算子は multiply, add, divide のどれかです')
   }
 }
 
