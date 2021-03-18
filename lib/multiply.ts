bfe92ce75ad0182addc40c5063f4b3ea70c62782
yoshikouki <yoshikouki@pepabo.com>
2021-03-18 15:19:02 +0900

    型安全な乗算機の完成

--- lib/multiply.ts
+++ lib/multiply.ts
@@ -1,8 +1,33 @@
+interface MultiplyValues {
+  value1: number
+  value2: number
+}
+
+const parseArguments = (args: Array<string>): MultiplyValues => {
+  if (args.length < 4) {
+    throw new Error('引数が足りません')
+  }
+  if (args.length > 4) {
+    throw new Error('引数が多すぎます')
+  }
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3])
+    }
+  } else {
+    throw new Error('引数には数字を指定してください')
+  }
+}
+
 const multiplicator = (a, b, printText) => {
   console.log(printText,  a * b);
 }
 
-const a: number = Number(process.argv[2])
-const b: number = Number(process.argv[3])
-
-multiplicator(a, b, `${a} と ${b} の乗算結果:`);
+try {
+  const { value1, value2 } = parseArguments(process.argv)
+  multiplicator(value1, value2, `${value1} と ${value2} の乗算結果:`);
+} catch (e) {
+  console.error('エラーが発生しました。メッセージ: ', e.message)
+}
