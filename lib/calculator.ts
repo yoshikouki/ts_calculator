bf8f8ef7d3be9846cc4d4fecbb9eeebb64bf4267
yoshikouki <yoshikouki@pepabo.com>
2021-03-18 14:57:45 +0900

    各計算処理を分離した

--- lib/calculator.ts
+++ lib/calculator.ts
@@ -23,10 +23,3 @@ try {
 } catch (e) {
   console.error('エラーが発生しました。メッセージ: ', e.message)
 }
-
-const multiplicator = (a, b, printText) => {
-  console.log(printText,  a * b);
-}
-
-multiplicator(2, 4, '2 と 4 の乗算結果:');
-multiplicator('string 型では？', 4, 'string と 4 の乗算結果:');
